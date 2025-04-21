101a0fe9ce429500b2049b8e1aa0885e1c4aa600
Md. Jahid Hasan <122719439+jahidhasanpiyesh@users.noreply.github.com>
2025-04-21 09:12:49 +0600

    Delete Day --05  Models across files directory

--- Day --05  Models across files/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import home
-from .first_models import first
-from .secend_models import second
-# Register your models here.
-
-admin.site.register(home)
-admin.site.register(first)
-admin.site.register(second)
