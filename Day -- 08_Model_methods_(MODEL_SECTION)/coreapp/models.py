8aeceabf1fd5dc60c2a9a0099635d3e1acacb18c
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2025-08-17 07:33:45 +0600

    Create Class base model and three field added.

--- Day -- 08_Model_methods_(MODEL_SECTION)/coreapp/models.py
+++ Day -- 08_Model_methods_(MODEL_SECTION)/coreapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
