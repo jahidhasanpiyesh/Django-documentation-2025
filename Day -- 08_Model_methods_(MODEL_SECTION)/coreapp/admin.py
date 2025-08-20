6aa839071799b4f1d8a63905966de52b1d87b3a6
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2025-08-20 09:38:54 +0600

    admin section show my project admin panal. Then most commonly use to list_display, search_field, list_filter .

--- Day -- 08_Model_methods_(MODEL_SECTION)/coreapp/admin.py
+++ Day -- 08_Model_methods_(MODEL_SECTION)/coreapp/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import person
 # Register your models here.
 # Show model file..
-admin.site.register(person)
+@admin.register(person)
+class personaladmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'full_name', 'birth_date', 'baby_status')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('birth_date',)
